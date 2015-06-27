be300d1ae5517065d00c3c081ecc46cc606f8c63
Mike Brown <mike.brown@excella.com>
2015-06-27 13:44:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# KHAN as a service, in Django
+
+  - **Technology stack**: 
+   - Python 2.7
+   - Django 1.8
+   - Postgres
+
+## Dependencies
+- **Vagrant / Virtualbox** - creates the VM used for development
+    Installation instructions can be found here: http://docs.vagrantup.com/v2/installation/
+
+## Installation
+
+Run `vagrant up` to create and provision the virtual machine
+
+### Command Line Access
+Once provisioning completes, connect to the machine using `vagrant ssh`
+
+### Starting / Stopping the VM
+To start the vagrant box: `vagrant up`  
+To stop the vagrant box: `vagrant halt`  
---
+++ provision/provision.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Script to set up a Django project on Vagrant.
+
+# Installation settings
+
+PROJECT_NAME=$1
+
+PROJECT_DIR=/vagrant/
+VIRTUALENV_DIR=/home/vagrant/.virtualenvs/$PROJECT_NAME
+
+PGSQL_VERSION=9.3
+
+# Install essential packages from Apt
+apt-get update -y
+# Python dev packages
+apt-get install -y build-essential python python-dev python-pip python-virtualenv
+apt-get install -y git
+
+if ! command -v workon; then
+    pip install virtualenvwrapper
+fi
+
+# Postgresql
+if ! command -v psql; then
+    apt-get install -y postgresql-$PGSQL_VERSION libpq-dev
+    cp /vagrant/provision/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
+    /etc/init.d/postgresql reload
+fi
+
+# bash environment global setup
+echo "export WORKON_HOME=/home/vagrant/.virtualenvs" >> /home/vagrant/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/vagrant/.bashrc
+
+# ---
+
+# postgresql setup for project
+createdb -Upostgres $PROJECT_NAME
+
+# virtualenv setup for project
+su - vagrant -c "/usr/bin/virtualenv $VIRTUALENV_DIR && \
+    echo $PROJECT_DIR > $VIRTUALENV_DIR/.project && \
+    $VIRTUALENV_DIR/bin/pip install -r $PROJECT_DIR/requirements.txt"
+
+echo "workon $PROJECT_NAME" >> /home/vagrant/.bashrc
